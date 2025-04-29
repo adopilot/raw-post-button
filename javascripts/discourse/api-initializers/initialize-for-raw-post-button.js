b7d74ce0adb5e8b88f41b6b3224132a840c17df5
Admir <adopilot@gmail.com>
2025-04-29 11:21:44 +0200

    sada

--- javascripts/discourse/api-initializers/initialize-for-raw-post-button.js
+++ javascripts/discourse/api-initializers/initialize-for-raw-post-button.js
@@ -20,7 +20,7 @@ export default apiInitializer("1.34.0", (api) => {
       context: { firstButtonKey,lastHiddenButtonKey, secondLastHiddenButtonKey },
     }) => {
       dag.add("show-raw", ShowRawButton, {
-        before: firstButtonKey
+        after: firstButtonKey
       });
     }
   );
